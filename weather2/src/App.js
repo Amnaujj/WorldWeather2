c2da1ea71463c5eebec662685d84667c87efa506
juan cuevas <juannmcuevas97@gmail.com>
2022-05-05 12:01:45 -0300

    1stCommit

---
+++ weather2/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Nav from './components/nav/Nav.jsx'
+import Cards from './components/cards/Cards.jsx'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Nav/>
+      <Cards/>
+    </div>
+  );
+}
+
+export default App;
